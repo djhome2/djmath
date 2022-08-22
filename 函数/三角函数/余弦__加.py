fa64d9b671e2e4dbb9a7f4b4d00201415fe2946c
dj <djesc@139.com>
2022-08-22 13:15:45 +0800

    cos(x+y)

--- 函数/三角函数/余弦__加.py
+++ 函数/三角函数/余弦__加.py
@@ -4,16 +4,17 @@ import __init__
 
 
 from 乘 import 乘
+from 减 import 减
 from 函数 import 函数
 from 变量 import 变量
 from 正弦 import 正弦
 from 余弦 import 余弦
 
-from 加 import 加
+
 from 实数 import 实数
 
 
-class 正弦__加(函数):
+class 余弦__加(函数):
 
     def __init__(self, 参数1, 参数2):
         函数.__init__(self, 参数1, 参数2)
@@ -22,17 +23,17 @@ class 正弦__加(函数):
     def get(self):
         a = self.get_x(self.x[0])
         b = self.get_x(self.x[1])
-        c1 = 乘(正弦(a), 余弦(b))
-        c2 = 乘(余弦(a), 正弦(b))
-        f = 加(c1, c2)
+        c1 = 乘(余弦(a), 余弦(b))
+        c2 = 乘(正弦(a), 正弦(b))
+        f = 减(c1, c2)
         return f.get()
 
 
 if __name__ == "__main__":
     # execute only if run as a script
-    f = 正弦__加(变量('x'), 变量('y'))
+    f = 余弦__加(变量('x'), 变量('y'))
     print(f)
-    f = 正弦__加(实数(1), 实数(2))
+    f = 余弦__加(实数(1), 实数(2))
     print(f)
-    f = 正弦__加(实数(1), 变量('a'))
+    f = 余弦__加(实数(1), 变量('a'))
     print(f)
